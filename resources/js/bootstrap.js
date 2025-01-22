dfc1ba83354a54eb2f282cb0fa02364309ba595a
Ridwan <ridwanpr@outlook.com>
2025-01-23 05:28:26 +0700

    remove unused code

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,8 +2,3 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-import { Notyf } from 'notyf';
-import 'notyf/notyf.min.css';
-
-import 'bootstrap-icons/font/bootstrap-icons.css';
