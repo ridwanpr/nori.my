ee38fb6d74e243c90815e254d77314339a99379b
Ridwan <ridwanpr@outlook.com>
2025-01-24 20:28:24 +0700

    delay disqus

--- resources/views/layouts/partials/disqus-comment.blade.php
+++ resources/views/layouts/partials/disqus-comment.blade.php
@@ -1,17 +1,19 @@
 <div id="disqus_thread"></div>
 <script>
-    const disqus_config = function() {
-        this.page.url = '{{ url()->current() }}';
-        this.page.identifier = '{{ request()->path() }}'
-    };
+    document.addEventListener('DOMContentLoaded', function() {
+        setTimeout(function() {
+            const disqus_config = function() {
+                this.page.url = '{{ url()->current() }}';
+                this.page.identifier = '{{ request()->path() }}'
+            };
 
-    (function() {
-        const d = document,
-            s = d.createElement('script');
-        s.src = 'https://nori-3.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
+            const d = document,
+                s = d.createElement('script');
+            s.src = 'https://nori-3.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        }, 1000);
+    });
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by
         Disqus.</a></noscript>
