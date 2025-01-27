4b6d6497ee3e73cfc66fd518f021d1f4e6fb9726
Ridwan <ridwanpr@outlook.com>
2025-01-28 03:56:18 +0700

    fix h1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <h1 style="visibility: hidden;">Nonton Anime Streaming Sub Indo Gratis - Nori</h1>
+    <h1 style="position: absolute; left: -9999px;">Nonton Anime Streaming Sub Indo - Nori</h1>
     <section class="trending-section mb-4">
         <div class="container-fluid px-3">
             <div class="d-flex justify-content-between align-items-center mb-3">
